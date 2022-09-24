9912f435322a3ac1ee5069893cc16d71223bceb7
Andrew Shopland <hi@ashp.land>
2022-09-23 17:20:05 -0700

    Initial Commit

---
+++ Shared/MoBikesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoBikesApp.swift
+//  Shared
+//
+//  Created by Andrew on 2022-09-23.
+//
+
+import SwiftUI
+
+@main
+struct MoBikesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
